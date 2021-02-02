f592f3aceb7dd530568b3b47c6d18c420c4074fc
Aaron Steinfeld <45047841+aaron-steinfeld@users.noreply.github.com>
2021-02-01 17:25:39 -0800

    feat: update hyperdash to bring in custom deserializers (#158)
    also includes bugfix for variable reference tracking cleanup on model
    destroy

--- projects/hyperdash-angular/src/injectable-wrappers/model-manager.service.ts
+++ projects/hyperdash-angular/src/injectable-wrappers/model-manager.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ModelManager } from '@hypertrace/hyperdash';
+import { BeforeModelDestroyedEventService } from './before-model-destroyed-event.service';
 import { LoggerService } from './logger.service';
 import { ModelCreatedEventService } from './model-created-event.service';
 import { ModelDestroyedEventService } from './model-destroyed-event.service';
@@ -12,8 +13,9 @@ export class ModelManagerService extends ModelManager {
   public constructor(
     logger: LoggerService,
     modelDestroyedEvent: ModelDestroyedEventService,
-    modelCreatedEvent: ModelCreatedEventService
+    modelCreatedEvent: ModelCreatedEventService,
+    beforeModelDestroyedEvent: BeforeModelDestroyedEventService
   ) {
-    super(logger, modelCreatedEvent, modelDestroyedEvent);
+    super(logger, modelCreatedEvent, modelDestroyedEvent, beforeModelDestroyedEvent);
   }
 }
