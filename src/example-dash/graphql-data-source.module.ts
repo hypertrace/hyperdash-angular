90b8d012eb1322ae07888bdbf52b6c7e877f6ec2
Adithya Sreyaj <adithya.sreyaj@traceable.ai>
2024-05-23 22:29:46 +0530

    chore: upgrade cli

--- src/example-dash/graphql-data-source.module.ts
+++ src/example-dash/graphql-data-source.module.ts
@@ -1,14 +1,14 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { DashboardCoreModule } from '@hypertrace/hyperdash-angular';
 import { GraphQlDataSource } from './graphql-data-source';
 
 @NgModule({
   imports: [
-    HttpClientModule,
     DashboardCoreModule.with({
       models: [GraphQlDataSource]
     })
-  ]
+  ],
+  providers: [provideHttpClient(withInterceptorsFromDi())]
 })
 export class GraphQlDataSourceModule {}
