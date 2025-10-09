db37422de814a78f54c0c046f678ef26a2dad5a9
Anand Tiwary <anand@traceable.ai>
2025-10-08 23:00:13 -0700

    refactor: update lint rules

--- projects/hyperdash-angular/src/editor/nested-model/nested-model-editor.component.ts
+++ projects/hyperdash-angular/src/editor/nested-model/nested-model-editor.component.ts
@@ -9,7 +9,7 @@ import { EDITOR_API } from '../editor-api-injection-token';
 @Component({
   selector: 'hda-nested-model-editor',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  template: ` <hda-model-json-editor [modelJson]="api.value" (modelJsonChange)="api.valueChange($event)" /> `,
+  template: '<hda-model-json-editor [modelJson]="api.value" (modelJsonChange)="api.valueChange($event)" /> ',
   standalone: false
 })
 export class NestedModelEditorComponent {
--- projects/hyperdash-angular/src/injectable-wrappers/injectable-wrappers.test.ts
+++ projects/hyperdash-angular/src/injectable-wrappers/injectable-wrappers.test.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/no-restricted-types */
 import { TestBed } from '@angular/core/testing';
 import {
   ArrayDeserializer,
