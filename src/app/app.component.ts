874939a8b16e230c5f1d3460f3f94587db9fc62a
Kevin Nagurski <kevin.nagurski@harness.io>
2026-01-29 14:45:09 +0000

    fix: update Angular to v20
    Update from Angular v19 to v20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { DashboardCoreModule, DashboardEditorModule } from 'dist/hyperdash-angular';
+import { DashboardCoreModule, DashboardEditorModule } from '@hypertrace/hyperdash-angular';
 import { ExampleDashModule } from '../example-dash/example-dash.module';
 import { GraphQlDataSourceModule } from '../example-dash/graphql-data-source.module';
 
@@ -11,7 +10,6 @@ import { GraphQlDataSourceModule } from '../example-dash/graphql-data-source.mod
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   imports: [
-    BrowserModule,
     FormsModule,
     CommonModule,
     GraphQlDataSourceModule,
